cb579156d1aee45bb1072a4dc9aca5d4175db6a1
Ioana Leontiuc <I.Leontiuc@student.tudelft.nl>
2017-03-21 16:40:35 +0100

    corect test

--- ChangeSettingDummy/src/main/java/ChangeSettingDummy/ChangeSettingDummy/BankAccount.java
+++ ChangeSettingDummy/src/main/java/ChangeSettingDummy/ChangeSettingDummy/BankAccount.java
@@ -9,7 +9,7 @@ public class BankAccount {
 		if (amount >=  0.00) { 
 			balance = balance - amount;
 			if(balance < 0.00){
-				isOverdraft = true;
+				isOverdraft = false;
 			}
 			return true; 
 		}else{
